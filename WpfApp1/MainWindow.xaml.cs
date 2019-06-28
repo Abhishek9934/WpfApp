8d4fb5ff7c090e181011d3e3ef0343af14d771c3
unknown <shivamprsd99@gmail.com>
2019-06-28 14:22:34 +0530

    ftp server created

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Windows.Controls;
 using System.Windows.Shapes;
 using WpfApp1.ViewModels;
+using System.Net;
 
 namespace WpfApp1
 {
@@ -26,15 +29,28 @@ namespace WpfApp1
             InitializeComponent();
         }
 
-        private void RedView_Clicked(object sender, RoutedEventArgs e)
-
+        byte[] ConvertImageToBinary(System.Drawing.Image img)
         {
-            DataContext = new RedViewModel();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
         }
+
+        System.Drawing.Image ConvertBinaryToImage(byte[] data)
+        {
+            using(MemoryStream ms = new MemoryStream(data))
+            {
+                return System.Drawing.Image.FromStream(ms);
+            }
+        }
+
 
         private void BlueView_Clicked(object sender, RoutedEventArgs e)
         {
             DataContext = new BlueViewModel();
         }
+
     }
 }
--- WpfApp1/Views/BlueView.xaml.cs
+++ WpfApp1/Views/BlueView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
 
 namespace WpfApp1.Views
 {
@@ -23,6 +26,63 @@ namespace WpfApp1.Views
         public BlueView()
         {
             InitializeComponent();
+            img2.Source = getImage();
+            img1.Source = getImage();
+        }
+
+        private BitmapImage getImage()
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://192.168.1.2/Fruit shapes.jpg");
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            request.Credentials = new NetworkCredential("microfluidics lab", "workstation");
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.StreamSource = responseStream;
+            bi.EndInit();
+            return bi;
+        }
+
+        private void uploadFile()
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://192.168.1.2/up.txt");
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential("microfluidics lab", "workstation");
+
+            // Copy the contents of the file to the request stream.
+            byte[] fileContents;
+            using (StreamReader sourceStream = new StreamReader("C:/Users/Microfluidics Lab/Desktop/up.txt"))
+            {
+                fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+            }
+
+            request.ContentLength = fileContents.Length;
+
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(fileContents, 0, fileContents.Length);
+            }
+        }
+
+        private string deleteFile()
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://192.168.1.2/up.txt");
+            request.Method = WebRequestMethods.Ftp.DeleteFile;
+
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential("microfluidics lab", "workstation");
+            using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+            {
+                return response.StatusDescription;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            uploadFile();
         }
     }
 }
